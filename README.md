60cb4f680724ce30a5ea7de5370ecfb5d68cbc2a
mehar116908 <meharbhatia07@gmail.com>
2021-01-11 23:57:06 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+## Output
+
+Output is the way the computer prints information the user in the "Console"
+
+To output we use `print`
+
+## Comments
+
+Comments are notes that the compiler skips over
+
+to comment we use `#`
---
+++ main.py
@@ -0,0 +1,7 @@
+print("Hello World") #this print statment prints hello world
+# write 5 print statements
+print("My name is Mehar Bhatia")
+print("I am in 7th Grade")
+print("My favorite color is Green")
+print("My favorite sport is Golf")
+print("My favorite ice cream flavor is Mint Chip")
